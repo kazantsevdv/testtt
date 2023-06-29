e10eb2d0cf1b2bfd8efaa00acd1884588136f6a5
Denis Kazantsev <denspa@mail.ru>
2023-06-29 12:51:38 +0300

    1

--- app/src/main/java/com/kazantcev/testtt/MainActivity.kt
+++ app/src/main/java/com/kazantcev/testtt/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //1
+        //2
+        //3
     }
 }
